2dbf7e250e5e52b994e5b1f6b36b295be57d1903
fabianchs <fabichasola@hotmail.com>
2021-03-03 23:00:16 +0000

    contadores con props

---
+++ src/js/component/counters.js
@@ -0,0 +1,52 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export function Counters(props) {
+	return (
+		<div className="text-center mx-auto  d-flex justify-content-center mt-5 ">
+			<span
+				className="badge bg-secondary text-light d-flex justify-content-center align-items-center border border-black"
+				id="cont1">
+				<i className="far fa-clock fa-4x"></i>
+			</span>
+			<span
+				className="badge bg-secondary text-light d-flex justify-content-center align-items-center border border-black"
+				id="cont1">
+				<h1 id="num">{props.cont6}</h1>
+			</span>
+			<span
+				className="badge bg-secondary text-light d-flex justify-content-center align-items-center border border-black"
+				id="cont1">
+				<h1 id="num">{props.cont5}</h1>
+			</span>
+			<span
+				className="badge bg-secondary text-light d-flex justify-content-center align-items-center border border-black"
+				id="cont1">
+				<h1 id="num">{props.cont4}</h1>
+			</span>
+			<span
+				className="badge bg-secondary text-light d-flex justify-content-center align-items-center border border-black"
+				id="cont1">
+				<h1 id="num">{props.cont3}</h1>
+			</span>
+			<span
+				className="badge bg-secondary text-light d-flex justify-content-center align-items-center border border-black"
+				id="cont1">
+				<h1 id="num">{props.cont2}</h1>
+			</span>
+			<span
+				className="badge bg-secondary text-light d-flex justify-content-center align-items-center border border-black"
+				id="cont1">
+				<h1 id="num">{props.cont1}</h1>
+			</span>
+		</div>
+	);
+}
+Counters.propTypes = {
+	cont1: PropTypes.string,
+	cont2: PropTypes.string,
+	cont3: PropTypes.string,
+	cont4: PropTypes.string,
+	cont5: PropTypes.string,
+	cont6: PropTypes.string
+};
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,47 +1,20 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
+import { Counters } from "./counters.js";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mx-auto  d-flex justify-content-center mt-5 ">
-			<span
-				className="badge bg-secondary text-light d-flex justify-content-center align-items-center border border-black"
-				id="cont1">
-				<i className="far fa-clock fa-4x"></i>
-			</span>
-			<span
-				className="badge bg-secondary text-light d-flex justify-content-center align-items-center border border-black"
-				id="cont1">
-				<h1 id="num">8</h1>
-			</span>
-			<span
-				className="badge bg-secondary text-light d-flex justify-content-center align-items-center border border-black"
-				id="cont1">
-				<h1 id="num">8</h1>
-			</span>
-			<span
-				className="badge bg-secondary text-light d-flex justify-content-center align-items-center border border-black"
-				id="cont1">
-				<h1 id="num">8</h1>
-			</span>
-			<span
-				className="badge bg-secondary text-light d-flex justify-content-center align-items-center border border-black"
-				id="cont1">
-				<h1 id="num">8</h1>
-			</span>
-			<span
-				className="badge bg-secondary text-light d-flex justify-content-center align-items-center border border-black"
-				id="cont1">
-				<h1 id="num">8</h1>
-			</span>
-			<span
-				className="badge bg-secondary text-light d-flex justify-content-center align-items-center border border-black"
-				id="cont1">
-				<h1 id="num">8</h1>
-			</span>
+		<div id="app">
+			<Counters
+				cont1="0"
+				cont2="0"
+				cont3="0"
+				cont4="0"
+				cont5="0"
+				cont6="0"
+			/>
 		</div>
 	);
 }
